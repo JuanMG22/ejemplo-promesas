7be70d8f9cc7cf4377f8937dbf8f8d21c43c2835
juanmg22 <juan.m-gonzalez@outlook.com>
2022-06-21 19:10:58 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+//Datos que se requieren obtener
+const productos = [
+  {
+    "precio": 27030,
+    "id": 1,
+    "titulo": "Procesador AMD Ryzen 5 2600",
+    "categoria": "ProcesadoresAMD",
+    "descripcion": "Procesador AMD 6 Núcleos 12 Hilos",
+    "imagen": "https://juanmg22.github.io/High-Gaming/img/cpus/cpu-1.jpg"
+  },
+  {
+    "precio": 28930,
+    "id": 2,
+    "titulo": "Procesador AMD Ryzen 5 3500X ",
+    "categoria": "ProcesadoresAMD",
+    "descripcion": "Procesador AMD 6 Núcleos 6 Hilos",
+    "imagen": "https://juanmg22.github.io/High-Gaming/img/cpus/cpu-2.jpg"
+  }
+]
+
+// const getDatos = () => {
+    
+// }
